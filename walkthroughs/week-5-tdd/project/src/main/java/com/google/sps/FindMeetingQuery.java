e688594e358cda0fe021dae9a3f234909f2490f9
Gabriel Stewart <glstewart17@gmail.com>
2020-06-17 00:55:51 +0000

    Finish find meeting query algorithm.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,68 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Iterator;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    // Save all attendees that should attend and the duration of the meeting.
+    Collection<String> attendees = request.getAttendees();
+    long duration = request.getDuration();
+
+    // Add all time ranges that someone who is in the meeting will be in to an arrayList.
+    ArrayList<TimeRange> busyTimeRanges = new ArrayList<TimeRange>();
+    Iterator<Event> eventsIterator = events.iterator();
+    while (eventsIterator.hasNext()) {
+      Event event = eventsIterator.next();
+      for (String eventAttendee: event.getAttendees())
+        if (attendees.contains(eventAttendee)) {
+          busyTimeRanges.add(event.getWhen());
+          break;  
+        }
+    }
+
+    // Sort the busy time ranges by start time.
+    Collections.sort(busyTimeRanges, TimeRange.ORDER_BY_START);
+
+    // While the start time is before the end of the day, continue adding possible meeting times. 
+    int startTime = TimeRange.START_OF_DAY;
+    ArrayList<TimeRange> possibleMeetingTimes = new ArrayList<TimeRange>();
+    while (startTime < TimeRange.END_OF_DAY) {
+      
+      // If no busy times remaining, add remaining window if it fits.
+      if (busyTimeRanges.size() == 0) {
+        if (startTime + duration <= TimeRange.END_OF_DAY) {
+          possibleMeetingTimes.add(TimeRange.fromStartEnd(startTime, TimeRange.END_OF_DAY, true));
+        }
+        startTime = TimeRange.END_OF_DAY;
+      }
+
+      // If there is a busy time that starts before the start time, find the next start time. 
+      else if (busyTimeRanges.get(0).start() < startTime) {
+        if (busyTimeRanges.get(0).end() > startTime) {
+          startTime = busyTimeRanges.get(0).end();
+        }
+        busyTimeRanges.remove(0);
+      }
+
+      // Otherwise, if there is a large enough window, add it, otherwise, find the next start time.
+      else {
+        if (startTime + duration <= busyTimeRanges.get(0).start()) {
+          possibleMeetingTimes.add(TimeRange.fromStartEnd(startTime, busyTimeRanges.get(0).start(), false)) ;
+        }
+        startTime = busyTimeRanges.get(0).end();
+        busyTimeRanges.remove(0);
+      }
+    } 
+
+    // Return all possible meeting times.
+    return possibleMeetingTimes;
   }
 }
